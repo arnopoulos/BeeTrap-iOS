1a57c1d536066c17a03c9ca17d215d795018a3cc
Andrew Arnopoulos <andrewarnopoulos@gmail.com>
2014-11-14 22:39:26 -0700

    Removed Table View added Wallet List

---
+++ Honeypot/WalletListViewController.h
@@ -0,0 +1,13 @@
+//
+//  WalletListViewController.h
+//  Honeypot
+//
+//  Created by Andrew Arnopoulos on 11/14/14.
+//  Copyright (c) 2014 Andrew Arnopoulos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WalletListViewController : UIViewController
+
+@end
---
+++ Honeypot/WalletListViewController.m
@@ -0,0 +1,37 @@
+//
+//  WalletListViewController.m
+//  Honeypot
+//
+//  Created by Andrew Arnopoulos on 11/14/14.
+//  Copyright (c) 2014 Andrew Arnopoulos. All rights reserved.
+//
+
+#import "WalletListViewController.h"
+
+@interface WalletListViewController ()
+
+@end
+
+@implementation WalletListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
