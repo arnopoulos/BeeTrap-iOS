24387bec9ae7ef66b74e9c1f832a9f89905da71c
Andrew Arnopoulos <andrewarnopoulos@gmail.com>
2014-11-14 21:37:50 -0700

    Added BeeTraps button

--- Honeypot/ViewController.swift
+++ Honeypot/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 	
-	@IBOutlet var addressLabel:UILabel?
+	@IBOutlet var addressLabel:UITextField!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
