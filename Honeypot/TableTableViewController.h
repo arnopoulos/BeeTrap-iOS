6f44939068f71db108dbc43ed7363a6dcfc8776d
Ala Park <AlaPark@Alas-MacBook-Pro.local>
2014-11-14 22:18:19 -0700

    Added Table View Controller

---
+++ Honeypot/TableTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableTableViewController.h
+//  Honeypot
+//
+//  Created by Ala Park on 11/14/14.
+//  Copyright (c) 2014 Andrew Arnopoulos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableTableViewController : UITableViewController
+
+@end
