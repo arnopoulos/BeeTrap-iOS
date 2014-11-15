d1e3f50f654e254f6174409fdc5b0ab41d736155
Andrew Arnopoulos <andrewarnopoulos@gmail.com>
2014-11-15 08:51:43 -0700

    Added JSON request

--- Honeypot/ViewController.swift
+++ Honeypot/ViewController.swift
@@ -8,20 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 	
-	@IBOutlet var addressLabel:UITextField!
+	@IBOutlet var addressLabel:UITextField! {
+		didSet {
+			addressLabel.delegate = self
+		}
+	}
+	
+	lazy var data = NSMutableData()
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+	func textFieldShouldReturn(textField: UITextField) -> Bool {
+		textField.resignFirstResponder()
+		textField.text = "";
+		return true
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	func textFieldDidEndEditing(textField: UITextField) {
+		startValidation(addressLabel.text)
 	}
-
-
+	
+	func startValidation(address:String) {
+		
+		let htmlTag = "?address=" + address
+		
+		let addressString = "https://bee-trap.herokuapp.com/users/2/wallet.json" + htmlTag
+//		let addressString = "http://www.arnopoulos.io/test"
+		println(addressString)
+		let url = NSURL(string: addressString)
+		let request  = NSMutableURLRequest()
+		request.URL = url
+		request.HTTPMethod = "GET"
+		
+		NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue()) { (response, data, error) -> Void in
+			if error == nil {
+				var err: AutoreleasingUnsafeMutablePointer<NSError?> = nil
+				let jsonResult = NSJSONSerialization.JSONObjectWithData(data!, options:NSJSONReadingOptions.MutableContainers, error: err) as? NSDictionary
+				
+				if let jsonResult = jsonResult {
+					println(jsonResult)
+				} else {
+					println("Cannot parse json.")
+					println(err)
+				}
+				
+			} else {
+				println(error)
+			}
+		}
+		
+		
+	}
+	
 }
 
