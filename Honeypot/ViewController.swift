88b05493851c3ebbd6297cbdb51a642faf2169ad
Andrew Arnopoulos <andrewarnopoulos@gmail.com>
2014-11-14 20:21:33 -0700

    Initial Commit

---
+++ Honeypot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Honeypot
+//
+//  Created by Andrew Arnopoulos on 11/14/14.
+//  Copyright (c) 2014 Andrew Arnopoulos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
