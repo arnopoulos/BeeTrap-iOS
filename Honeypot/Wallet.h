a3601fc67611dfbe44237e9206105b75b96ba83b
dria7226 <dria7226@colorado.edu>
2014-11-15 08:43:51 -0700

    Wallet List View Controller

---
+++ Honeypot/Wallet.h
@@ -0,0 +1,13 @@
+//
+//  Wallet.h
+//  Honeypot
+//
+//  Created by Ala Park on 11/14/14.
+//  Copyright (c) 2014 Andrew Arnopoulos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Wallet : NSObject
+
+@end
---
+++ Honeypot/Wallet.m
@@ -0,0 +1,13 @@
+//
+//  Wallet.m
+//  Honeypot
+//
+//  Created by Ala Park on 11/14/14.
+//  Copyright (c) 2014 Andrew Arnopoulos. All rights reserved.
+//
+
+#import "Wallet.h"
+
+@implementation Wallet
+
+@end
